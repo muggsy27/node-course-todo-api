92eed99a3567da14dafc7ce17fcc7cc5c3acd176
Derek Fletes <derekfletes@gmail.com>
2018-02-06 00:08:47 -0800

    Add delete script

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -32,7 +32,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, client) => {
   //   location: 'Pacific Beach'
   // }, (error, result) => {
   //   if (error) {
-  //     return console.log('unable to isnert todo');
+  //     return console.log('unable to insert todo');
   //   }
   //
   //   console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
@@ -54,10 +54,28 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, client) => {
   //   console.log('unable to fetch todos', error);
   // });
 
-  db.collection('Users').find({name: 'Derek'}).toArray().then((docs) => {
-    console.log(JSON.stringify(docs, undefined, 2));
-  }, (error) => {
-    console.log('unable to fetch todos', error);
+  // db.collection('Users').find({name: 'Derek'}).toArray().then((docs) => {
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (error) => {
+  //   console.log('unable to fetch todos', error);
+  // });
+
+  // db.collection('Todos').deleteOne({text: 'eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+  //   console.log(result);
+  // });
+
+  // db.collection('Users').deleteMany({name: 'Derek'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndDelete({
+    _id: new ObjectID('5a79602ad13bbb032b3cf614')
+  }).then((result) => {
+    console.log(result);
   });
 
   client.close();
